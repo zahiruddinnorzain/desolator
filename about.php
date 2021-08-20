7478586b6996f1d73c84e84c143c80ef8be42015
zahiruddin <kepaknaga@gmail.com>
2021-08-20 14:41:00 +0800

    init

---
+++ about.php
@@ -0,0 +1,21 @@
+<?php include 'top_html.php' ?>
+<body>
+<?php include 'navbar.php' ?>
+
+<!-- start body -->
+
+<div class="container-fluid">
+   <div class="row">
+      <div class="col-sm-4">
+
+         this is about page
+
+      </div>
+   </div>
+</div>
+
+<!-- end body -->
+
+<?php include 'footer.php' ?>
+</body>
+<?php include 'end_html.php' ?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include 'top_html.php' ?>
+<body>
+<?php include 'navbar.php' ?>
+
+<!-- start body -->
+
+<div class="container-fluid">
+   <div class="row">
+      <div class="col-sm-4">
+
+         ini body content
+         <a href="about.php">about</a>
+
+      </div>
+   </div>
+</div>
+
+<!-- end body -->
+
+<?php include 'footer.php' ?>
+</body>
+<?php include 'end_html.php' ?>
